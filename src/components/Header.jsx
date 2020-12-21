b525f59ba7ca61494cf0354f14c732ca79fcc06d
Mykola Rubashnyi <rubashnyimykola@gmail.com>
2020-12-21 11:13:53 +0200

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"
+                alt="logo"/>
+
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://oecdenvironmentfocusblog.files.wordpress.com/2020/06/wed-blog-shutterstock_1703194387_low_nwm.jpg?w=640"
+                    alt="nature"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>
+                New posts
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+
+    )
+}
+export default Profile;
