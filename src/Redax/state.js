7f1595a56002d280e7a45bf9d228a420883cff64
Mykola Rubashnyi <rubashnyimykola@gmail.com>
2021-01-20 11:56:52 +0200

    pravki v createPostFunc

--- src/Redax/state.js
+++ src/Redax/state.js
@@ -1,78 +1,98 @@
-import {renderEntireTree} from "../render";
+let store = {
+    idPost: 0,
+    createPostFunc(message, likesCount) {
+        return {
+            id: this.idPost++,
+            message: this.message,
+            likesCount: this.likesCount
+        }
+    },
+    createMessageFunc(messages) {
+        return {
+            id: this.idPost++,
+            message: this.messages
+        }
+    },
+    _state: {
+        profilePage: {
+            posts: [
+                this.createPostFunc('Zalupa sdfsdf', 26),
+                this.createPostFunc('Lolka', 55),
+                this.createPostFunc('Komen', 66),
+            ],
+            newPostText: 'React-First-Post'
+        },
 
-let idPost = 0;
-let createPostFunc = function (message, likesCount) {
-    return {
-        id: idPost++,
-        message: message,
-        likesCount: likesCount
-    }
-}
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Misha'},
+                {id: 2, name: 'Grisha'},
+                {id: 3, name: 'Vova'},
+                {id: 4, name: 'Lera'},
+                {id: 5, name: 'Kolya'},
+                {id: 6, name: 'Valera'}
+            ],
+            messages: [
+                this.createMessageFunc('Hello'),
+                this.createMessageFunc('My'),
+                this.createMessageFunc('Dear'),
+                this.createMessageFunc('Friend'),
+                this.createMessageFunc('Hello'),
+            ],
+            newMessageText: 'Write your think'
+        }
+    },
+    getState() {
+        return this._state;
+    },
+    _callSubscriber() {
+        console.log('Some changed');
+    },
+    addPost() {
+        let newPost = this.createPostFunc(this._state.profilePage.newPostText, 5)
 
-let createMessageFunc = function (messages) {
-    return {
-        id: idPost++,
-        message: messages
-    }
-}
+        this._state.profilePage.posts.push(newPost);
+        this._state.updateNewPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    addMessage() {
+        let newMessage = this.createMessageFunc(this._state.dialogsPage.newMessageText)
 
-let state = {
-    profilePage: {
-        posts: [
-            createPostFunc('Zalupa sdfsdf', 26),
-            createPostFunc('Lolka', 55),
-            createPostFunc('Komen', 66),
-        ],
-        newPostText: 'React-First-Post'
+        this._state.dialogsPage.messages.push(newMessage);
+        this._state.dialogsPage.newMessageText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewMessageText(newMessage) {
+        this._state.dialogsPage.newMessageText = newMessage;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
     },
 
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: 'Misha'},
-            {id: 2, name: 'Grisha'},
-            {id: 3, name: 'Vova'},
-            {id: 4, name: 'Lera'},
-            {id: 5, name: 'Kolya'},
-            {id: 6, name: 'Valera'}
-        ],
-        messages: [
-            createMessageFunc('Hello'),
-            createMessageFunc('My'),
-            createMessageFunc('Dear'),
-            createMessageFunc('Friend'),
-            createMessageFunc('Hello'),
-        ],
-        newMessageText: 'Write your think'
-    }
+
 }
 
+// let idPost = 0;
+// let createPostFunc = function (message, likesCount) {
+//     return {
+//         id: idPost++,
+//         message: message,
+//         likesCount: likesCount
+//     }
+// }
+//
+// let createMessageFunc = function (messages) {
+//     return {
+//         id: idPost++,
+//         message: messages
+//     }
+// }
 
-export let addPost = () => {
-    let newPost = createPostFunc(state.profilePage.newPostText, 5)
-
-    state.profilePage.posts.push(newPost);
-    state.updateNewPostText = '';
-    renderEntireTree(state);
-}
-
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    renderEntireTree(state);
-}
-
-export let addMessage = () => {
-    let newMessage = createMessageFunc(state.dialogsPage.newMessageText)
-
-    state.dialogsPage.messages.push(newMessage);
-    state.dialogsPage.newMessageText = '';
-    renderEntireTree(state);
-}
-
-export let updateNewMessageText = (newMessage) => {
-    state.dialogsPage.newMessageText = newMessage;
-    renderEntireTree(state);
-}
-
-export default state;
+export default store;
 
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {renderEntireTree} from "./render";
-import state from "./Redax/state";
+import state, {subscribe} from "./Redax/state";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import store from "./Redax/state";
 
-renderEntireTree(state);
+let renderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <React.StrictMode>
+                <App state={state}
+                     addPost={store.addPost.bind(store)}
+                     updateNewPostText={store.updateNewPostText.bind(store)}
+                     addMessage={store.addMessage.bind(store)}
+                     updateNewMessageText={store.updateNewMessageText.bind(store)}/>
+            </React.StrictMode>,
+        </BrowserRouter>,
+        document.getElementById('root'));
+}
+
+
+renderEntireTree(store.getState());
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+store.subscribe(renderEntireTree);
+
